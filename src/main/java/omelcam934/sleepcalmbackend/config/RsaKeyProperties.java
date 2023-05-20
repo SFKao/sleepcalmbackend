d90d0bb04d05492d9d098411c662a75ff3f68053
Oscar <oscarmeleroxd@gmail.com>
2023-05-20 11:48:45 +0200

    Cambiando a JWT

---
+++ src/main/java/omelcam934/sleepcalmbackend/config/RsaKeyProperties.java
@@ -0,0 +1,11 @@
+package omelcam934.sleepcalmbackend.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+    
+}
--- src/main/java/omelcam934/sleepcalmbackend/controller/AuthController.java
+++ src/main/java/omelcam934/sleepcalmbackend/controller/AuthController.java
@@ -1,20 +1,9 @@
 package omelcam934.sleepcalmbackend.controller;
 
 
-import omelcam934.sleepcalmbackend.dto.LoginDto;
-import omelcam934.sleepcalmbackend.dto.RegistroDto;
-import omelcam934.sleepcalmbackend.models.User;
-import omelcam934.sleepcalmbackend.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import omelcam934.sleepcalmbackend.service.TokenService;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,55 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    public AuthController(@Autowired UserService userService) {
-        this.userService = userService;
+    private TokenService tokenService;
+
+    public AuthController(TokenService tokenService) {
+        this.tokenService = tokenService;
     }
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-
-    private UserService userService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @PostMapping("/login")
-    public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto){
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                loginDto.getUsernameOrEmail(), loginDto.getPassword()));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>("Login del usuario correcto.", HttpStatus.OK);
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        String token = tokenService.generateToken(authentication);
+        return token;
     }
-
-    @PostMapping("/registro")
-    public ResponseEntity<?> registerUser(@RequestBody RegistroDto registroDto){
-
-        // add check for username exists in a DB
-        if(userService.existsByUsernameLikeIgnoreCase(registroDto.getUsername())){
-            return new ResponseEntity<>("Username is already taken!", HttpStatus.BAD_REQUEST);
-        }
-
-        // add check for email exists in DB
-        if(userService.existsByEmailLikeIgnoreCase(registroDto.getEmail())){
-            return new ResponseEntity<>("Email is already taken!", HttpStatus.BAD_REQUEST);
-        }
-
-        // create user object
-        User user = new User();
-        user.setUsername(registroDto.getUsername());
-        user.setUsername(registroDto.getUsername());
-        user.setEmail(registroDto.getEmail());
-        user.setPassword(passwordEncoder.encode(registroDto.getPassword()));
-
-        userService.save(user);
-
-        return new ResponseEntity<>("Usuario registrado correctamente.", HttpStatus.OK);
-
-    }
-
-
-
-
 }
